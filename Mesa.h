7c03424b1d682e384802e5364fd32f437a6d0d8f
Juan Jose Atuesta Flor <juanjoseatuestaflor@gmail.com>
2025-06-02 17:47:16 -0500

    commit de prueba

---
+++ Mesa.h
@@ -0,0 +1,11 @@
+#ifndef MESA_H
+#define MESA_H
+#include <vector>
+#include "Jugador.h"
+using namespace std;
+class Mesa {
+    private:
+        vector<Jugador> mesa;
+    public:
+
+adsfasdfsadf
